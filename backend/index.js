ad08ba8583a0a70531ff28f10699297ec2895231
msebbane <msebbane@Transcendance.myguest.virtualbox.org>
2024-04-19 15:32:10 +0200

    ajout nginx et HTTPS

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 // index.js
-
+/*
 const express = require('express');
 const app = express();
 const port = 8080;
@@ -10,4 +10,24 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Serveur démarré sur le port ${port}`);
+});*/
+
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+
+const app = express();
+
+const privateKey = fs.readFileSync('./nginx/localhost.key', 'utf8');
+const certificate = fs.readFileSync('./nginx/localhost.crt', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+const httpsServer = https.createServer(credentials, app);
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+httpsServer.listen(8080, () => {
+    console.log('Server running on port 8080');
 });
