12c6690956c6502c97ee1db76dc32a09a1e45fd7
msebbane <msebbane@Transcendance.myguest.virtualbox.org>
2024-04-22 20:21:08 +0200

    changement frontend avec standalone + ajout component login

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/Login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
