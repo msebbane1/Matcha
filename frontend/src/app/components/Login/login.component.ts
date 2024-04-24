4599986544ef4a90509ad629c848459f5e98fd47
msebbane <msebbane@Transcendance.myguest.virtualbox.org>
2024-04-24 22:56:36 +0200

    ajout page d'accueil test FRONT

--- frontend/src/app/components/Login/login.component.ts
+++ frontend/src/app/components/Login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef, AfterViewInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-login',
@@ -7,6 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent implements AfterViewInit {
+
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    document.addEventListener('mousemove', (event) => {
+      const moveWithMouse = this.el.nativeElement.querySelector('.move-with-mouse');
+      const mouseX = event.clientX;
+      const mouseY = event.clientY;
+      
+      const moveX = (mouseX / window.innerWidth) * 20 - 5;
+      const moveY = (mouseY / window.innerHeight) * 20 - 5;
+      
+      if (moveWithMouse) {
+        this.renderer.setStyle(moveWithMouse, 'transform', `translate(${moveX}px, ${moveY}px)`);
+      }
+    });
+  }
 }
